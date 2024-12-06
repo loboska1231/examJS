686f6487fc006ade695ec5d3244dea99cf47503c
loboska1231 <suskovboris@gmail.com>
2024-12-06 21:13:17 +0200

    type3

--- examJS/script.js
+++ examJS/script.js
@@ -5,20 +5,57 @@ let output = document.getElementById('pairList');
 let deleteButton = document.getElementById('delete');
 addButton.onclick = function (){
     let input = document.getElementById('pair');
-    let target =document.createElement('p');
-    target.innerText=input.value + '\n';
-    target.classList.add('item');
-    output.appendChild(target);
-    storage.push(input.value);
-    // заготовочка под локал стораге
-    localStorage.setItem('pairList',JSON.stringify(storage));
-    input.value='';
+    let str = input.value.match(/\s*\w+\s*=\s*\w+\s*/g)+'';
+    console.log(str,input.value)
+    if(input.value === str){
+        let temp = input.value.split('=');
+        let target = document.createElement('p');
+        let obj = {
+            name : temp[0],
+            value : temp[1]
+        };
+        if(storage.find(el=>(el.name === obj.name))) {
+            input.placeholder = 'ERROR';
+        }
+        else {
+            target.innerText=obj.name+'='+obj.value;
+            output.appendChild(target);
+            target.classList.add('item');
+            storage.push(obj);
+            localStorage.setItem('pairList', JSON.stringify(storage));
+            input.placeholder = '';
+        }
+    }
+    else input.placeholder = 'ERROR';
+    input.value = '';
+}
+output.onclick = function(ev){
+    let selectedTarget = ev.target;
+    if(selectedTarget.classList.contains('item'))
+        selectedTarget.classList.toggle('selected');
+}
+window.onload = function (){
+    for(let el of storage){
+        let p = document.createElement('p');
+        p.classList.add('item');
+        p.innerText = el.name+'='+el.value;
+        output.appendChild(p);
+    }
 }
 // нужно додумать как выделять объект
 deleteButton.onclick = function (ev){
-//    if(output.clinld?){
-//        array items = document.querySelectorAll('.item.selected?');
-//        items.for( item.delete)
-//    }
-//    else 'err'
+    if(output.hasChildNodes()){
+        let items = Array.from(document.querySelectorAll('.item.selected'));
+        for (const item of items.values()) {
+            let list = JSON.parse(localStorage.getItem('pairList'));
+            item.remove();
+            let itemName = item.innerText.split('=')[0];
+            let result = [];
+            list.forEach(el=>{
+                if(el.name!==itemName) result.push(el);
+            });
+            localStorage.setItem('pairList',JSON.stringify(result));
+        }
+    }
+    else console.log('err');
 }
